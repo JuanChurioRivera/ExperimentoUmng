2f7f6d0c273d9f7bbecf1c546a509d54dc6a4bec
JuanChurio <juanf346@hotmail.com>
2024-03-13 10:57:45 -0500

    back db

--- webExperimento/settings.py
+++ webExperimento/settings.py
@@ -75,19 +75,11 @@ WSGI_APPLICATION = 'webExperimento.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'sql_server.pyodbc',
-    'NAME': 'mydb',
-    'USER': 'JuanChurio',
-    'PASSWORD': 'UmngExperimento4$',
-    'HOST': 'umng-experiment.database.windows.net',
-    'PORT': '',
-
-    'OPTIONS': {
-        'driver': 'ODBC Driver 17 for SQL Server',
-    },
-
-},
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+       
+    }
 }
 
 
